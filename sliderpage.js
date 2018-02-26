a72d6c80415d6dd9f201a21e269d5c6567bbadf0
Abizar Bagasrawala <Abizar@TherapyOSs-MacBook-Pro.local>
2018-02-25 21:57:48 -0800

    committing

--- sliderpage.js
+++ sliderpage.js
@@ -3,6 +3,7 @@ var images = [];
 var imageHolder = document.getElementById("bitmoji-image");
 
 function init(){
+  document.write("working");
   if(screen.width <= 500)
     document.getElementById("descrOfDay").style.fontSize = "10px";
 }
