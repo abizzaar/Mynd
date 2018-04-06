0f14ff305c8d059e0039464bc00ce6981102b7e6
Abizar Bagasrawala <Abizar@dhcp-10-105-216-219.wireless.northwestern.private>
2018-04-05 20:44:52 -0500

    slider scaling better

--- sliderpage.js
+++ sliderpage.js
@@ -78,7 +78,7 @@ function init(){
   "use strict"; // Start of use strict
 
   $("#slider").roundSlider({
-    radius: 0.35 * screen.width,
+    radius: 0.35 * document.documentElement.clientWidth,
     circleShape: "half-bottom",
     sliderType: "min-range",
     showTooltip: false,
